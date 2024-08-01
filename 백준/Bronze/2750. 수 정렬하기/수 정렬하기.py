0b5290aabe41d5f93286a37b9fac68e3511ede11
hyeonji-cho <89562865+hyeonji-cho@users.noreply.github.com>
2024-08-02 08:33:37 +0900

    Update 수 정렬하기.py

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,9 +1,14 @@
 arr = []
-N = int(input())
+N = int(input()) # 수의 개수
+
+# 수 입력받기
 for i in range(N):
     m = int(input())
     arr.append(m)
-    
-arr.sort()
+
+# 오름차순 정렬
+arr.sort() 
+
+# 정렬된 결과 출력 
 for i in arr:
     print(i)
